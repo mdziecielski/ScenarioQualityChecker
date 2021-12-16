e4440db350964d765f402e0f6fcb1424ec482d2e
wsmcrn <zs1cyranik@gmail.com>
2021-12-16 23:36:26 +0100

    Implement AllStepCountingVisitor methods

--- src/main/java/pl/put/poznan/checker/logic/AllStepCountingVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/AllStepCountingVisitor.java
@@ -1,13 +1,18 @@
 package pl.put.poznan.checker.logic;
 
-public class AllStepCountingVisitor implements ScenarioVisitor{
+public class AllStepCountingVisitor implements ScenarioVisitor {
     @Override
-    public int visitSimpleStep(SimpleStep simpleStep) {
-        return 0;
+    public int visit(SimpleStep simpleStep) {
+        return 1;
     }
 
     @Override
-    public int visitComplexStep(ComplexStep complexStep) {
-        return 0;
+    public int visit(ComplexStep complexStep) {
+        int stepCount = 0;
+        AllStepCountingVisitor scv = new AllStepCountingVisitor();
+        for (Step st : complexStep.subscenario) {
+            stepCount += st.accept(scv);
+        }
+        return stepCount + 1;
     }
 }
