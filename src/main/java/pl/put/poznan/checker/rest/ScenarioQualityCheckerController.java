c7f6947bc2047cd66d286674aad06267dc4adc45
wsmcrn <szymon.cyranik@gmail.com>
2021-12-16 02:30:05 +0100

    Improve REST API implementation structure to handle webhook paths without necessary parameters e.g. '/getScenarioName'; API post call on test webhook now accepts TestClass object JSON Structure and returns the same object as JSON;

--- src/main/java/pl/put/poznan/checker/rest/ScenarioQualityCheckerController.java
+++ src/main/java/pl/put/poznan/checker/rest/ScenarioQualityCheckerController.java
@@ -1,42 +1,47 @@
 package pl.put.poznan.checker.rest;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.tomcat.util.json.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.checker.logic.ScenarioQualityChecker;
+import pl.put.poznan.checker.logic.TestClass;
 
 import java.util.Arrays;
 
 
 @RestController
-@RequestMapping("/{text}")
+@RequestMapping(value = {"/getScenarioName/{id}", "/getScenarioName"})
+
 public class ScenarioQualityCheckerController {
 
     private static final Logger logger = LoggerFactory.getLogger(ScenarioQualityCheckerController.class);
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
+    public String get(@PathVariable(value = "id", required = false) String id,
                               @RequestParam(value="checks", defaultValue="upper,escape") String[] checks) {
 
         // log the parameters
-        logger.debug(text);
+        logger.debug(id);
         logger.debug(Arrays.toString(checks));
 
         // perform the transformation, you should run your logic here, below is just a silly example
         ScenarioQualityChecker checker = new ScenarioQualityChecker(checks);
-        return checker.checks(text);
+        return checker.checks(id + " get");
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] checks) {
+    public TestClass post(@PathVariable(value = "id", required = false) String id,
+                          @RequestBody TestClass checks) {
 
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(checks));
+        logger.debug(id);
+        logger.debug(checks.name);
 
         // perform the transformation, you should run your logic here, below is just a silly example
-        ScenarioQualityChecker checker = new ScenarioQualityChecker(checks);
-        return checker.checks(text);
+//        ScenarioQualityChecker checker = new ScenarioQualityChecker(checks);
+//        return checker.checks(text + " post");
+        return checks;
     }
 
 
