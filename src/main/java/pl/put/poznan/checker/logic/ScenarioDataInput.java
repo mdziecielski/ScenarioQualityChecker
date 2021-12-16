e64da6e4a2f5515762fec0063feeb5d6ca2936af
wsmcrn <szymon.cyranik@gmail.com>
2021-12-16 11:52:10 +0100

    Add ScenarioDataInput - class that contains data received from input json request

---
+++ src/main/java/pl/put/poznan/checker/logic/ScenarioDataInput.java
@@ -0,0 +1,9 @@
+package pl.put.poznan.checker.logic;
+
+public class ScenarioDataInput {
+    public String title;
+    public String[] actors;
+    public String systemActor;
+
+    public Step[] steps;
+}
