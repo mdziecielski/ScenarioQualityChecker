40277e71d268090562cb8bdc4f33611815d114d3
wsmcrn <szymon.cyranik@gmail.com>
2021-12-16 11:50:35 +0100

    Add Step Interface

---
+++ src/main/java/pl/put/poznan/checker/logic/Step.java
@@ -0,0 +1,7 @@
+package pl.put.poznan.checker.logic;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "className")
+public interface Step {
+}
