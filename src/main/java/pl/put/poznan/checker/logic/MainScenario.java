83fec63a16f5fb2128125ab3ecea84851d2a8970
wsmcrn <zs1cyranik@gmail.com>
2021-12-16 23:37:20 +0100

    Minor changes to the implementation

---
+++ src/main/java/pl/put/poznan/checker/logic/MainScenario.java
@@ -0,0 +1,13 @@
+package pl.put.poznan.checker.logic;
+
+/**
+ * Scenario
+ */
+public class MainScenario {
+
+    public String title;
+    public String[] actors;
+    public String systemActor;
+
+    public Step[] steps;
+}
