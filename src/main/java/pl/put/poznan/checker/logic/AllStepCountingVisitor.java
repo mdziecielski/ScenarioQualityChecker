2e3c280f4f24000fdac75cf1525e7efe19c16e28
wsmcrn <zs1cyranik@gmail.com>
2021-12-17 12:54:24 +0100

    Remove redundant object
    More details [here](https://github.com/mdziecielski/ScenarioQualityChecker/pull/3#pullrequestreview-835050087)

--- src/main/java/pl/put/poznan/checker/logic/AllStepCountingVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/AllStepCountingVisitor.java
@@ -9,10 +9,19 @@ public class AllStepCountingVisitor implements ScenarioVisitor {
     @Override
     public int visit(ComplexStep complexStep) {
         int stepCount = 0;
-        AllStepCountingVisitor scv = new AllStepCountingVisitor();
         for (Step st : complexStep.subscenario) {
-            stepCount += st.accept(scv);
+            stepCount += st.accept(this);
         }
         return stepCount + 1;
+    }
+
+    @Override
+    public int visit(MainScenario mainScenario) {
+        int stepCount = 0;
+
+        for (Step step : mainScenario.steps) {
+            stepCount += step.accept(this);
+        }
+        return stepCount;
     }
 }
