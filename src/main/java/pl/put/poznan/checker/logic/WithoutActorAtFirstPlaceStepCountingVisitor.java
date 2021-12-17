77395b17e971fcaa014d546bdd1cfa4c4ec575c2
wsmcrn <zs1cyranik@gmail.com>
2021-12-17 21:26:45 +0100

    Change the way visitors are implemented to be compliant with new visitor structure

--- src/main/java/pl/put/poznan/checker/logic/WithoutActorAtFirstPlaceStepCountingVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/WithoutActorAtFirstPlaceStepCountingVisitor.java
@@ -2,17 +2,17 @@ package pl.put.poznan.checker.logic;
 
 public class WithoutActorAtFirstPlaceStepCountingVisitor implements ScenarioVisitor {
     @Override
-    public int visit(SimpleStep simpleStep) {
-        return 0;
+    public void visit(SimpleStep simpleStep) {
+        // return 0;
     }
 
     @Override
-    public int visit(ComplexStep complexStep) {
-        return 0;
+    public void visit(ComplexStep complexStep) {
+        // return 0;
     }
 
     @Override
-    public int visit(MainScenario mainScenario) {
-        return 0;
+    public void visit(MainScenario mainScenario) {
+        // return 0;
     }
 }
