3646bc46c4697f486b767becd05d117bd965d561
Michał Fredrych <70175772+Drske@users.noreply.github.com>
2021-12-21 13:47:48 +0100

    Add javadoc comments

--- src/main/java/pl/put/poznan/checker/logic/KeywordStepCountingVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/KeywordStepCountingVisitor.java
@@ -1,12 +1,26 @@
 package pl.put.poznan.checker.logic;
 
+/**
+ * Visitor which will count the total number of steps which start from a keyword.
+ */
+
 public class KeywordStepCountingVisitor implements ScenarioVisitor {
-    private String[] keywords = {"ELSE", "IF", "FOR EACH"};
+    /**
+     * List of keywords to look for.
+     */
+    private final String[] keywords = {"ELSE", "IF", "FOR EACH"};
+
+    /**
+     * Total count of steps containing any keyword.
+     */
     public int stepCount = 0;
 
-
+    /**
+     * Checks weather certain text (String) begins with any of the defined keywords.
+     * @param text Text of a step to check.
+     * @return Returns True when text begins with defined keywords, false otherwise
+     */
     public boolean  checkIfBeginWithKeyword(String text){
-        String[] splitStep = text.split("[ :]");
         for (String keyword : this.keywords){
             if (text.startsWith(keyword)){
                 return true;
@@ -15,12 +29,24 @@ public class KeywordStepCountingVisitor implements ScenarioVisitor {
         return false;
     }
 
+    /**
+     * For a SimpleStep Class Object, this method will increment stepCount by one
+     * when step starts with any of the defined keywords.
+     * @param simpleStep A SimpleStep Class object, step without sub-scenario
+     */
+
     @Override
     public void visit(SimpleStep simpleStep) {
         if (checkIfBeginWithKeyword(simpleStep.text)){
             this.stepCount += 1;
         }
     }
+
+    /**
+     * For a ComplexStep Class Object, this method will increment stepCount by one
+     * when step start with any of the defined keywords and visit all the steps in a sub-scenario.
+     * @param complexStep A ComplexStep Class object, step containing a sub-scenario
+     */
 
     @Override
     public void visit(ComplexStep complexStep) {
@@ -32,6 +58,12 @@ public class KeywordStepCountingVisitor implements ScenarioVisitor {
             step.accept(this);
         }
     }
+
+    /**
+     * For a MainScenario Class Object, this method will visit all the
+     * steps of a scenario.
+     * @param mainScenario Scenario to be analysed
+     */
 
     @Override
     public void visit(MainScenario mainScenario) {
--- src/main/java/pl/put/poznan/checker/logic/ScenarioVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/ScenarioVisitor.java
@@ -1,9 +1,26 @@
 package pl.put.poznan.checker.logic;
 
+/**
+ * Interface used by Visitor classes.
+ * Certain Visitors will be used to conduct actions on Scenario, and it's subtypes.
+ */
+
 public interface ScenarioVisitor {
+    /**
+     * This method defines actions conducted on SimpleStep Class Objects.
+     * @param simpleStep A SimpleStep Class object, step without sub-scenario
+     */
     void visit(SimpleStep simpleStep);
 
+    /**
+     * This method defines actions conducted on ComplexStep Class Objects.
+     * @param complexStep A ComplexStep Class object, step containing a sub-scenario
+     */
     void visit(ComplexStep complexStep);
 
+    /**
+     * This method defines actions conducted on MainScenario Class Obejcts.
+     * @param mainScenario Scenario to be analysed
+     */
     void visit(MainScenario mainScenario);
 }
