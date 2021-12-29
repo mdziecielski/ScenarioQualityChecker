ad94d0733dd1146d4b06842c857a75e6200c1725
Dawid Bosy <dawid.bosy@student.put.poznan.pl>
2021-12-29 18:46:09 +0100

    ActorsOccurencesCounter Implementation
    Added basic implementation of ActorsOccurencesCounter

---
+++ src/main/java/pl/put/poznan/checker/logic/ActorsOccurencesCounterOutput.java
@@ -0,0 +1,11 @@
+package pl.put.poznan.checker.logic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ActorsOccurencesCounterOutput {
+    public HashMap<String,Integer> actorsOccurences;
+
+    public ActorsOccurencesCounterOutput(HashMap<String,Integer> st) {actorsOccurences = st;
+    }
+}
