bedc3c454f9cce25f6bcd320865107dcb71a912a
mdziecielski <dziecielski.m@gmail.com>
2022-01-07 13:31:46 +0100

    Implement scenario depth counter

---
+++ src/main/java/pl/put/poznan/checker/logic/DepthCountOutput.java
@@ -0,0 +1,9 @@
+package pl.put.poznan.checker.logic;
+
+public class DepthCountOutput {
+    public int depthCount;
+
+    public DepthCountOutput(int d) {
+        depthCount = d;
+    }
+}
---
+++ src/main/java/pl/put/poznan/checker/logic/DepthCountingVisitor.java
@@ -0,0 +1,23 @@
+package pl.put.poznan.checker.logic;
+
+public class DepthCountingVisitor implements ScenarioVisitor {
+    int depthCount = 1;
+
+    @Override
+    public void visit(SimpleStep simpleStep) { }
+
+    @Override
+    public void visit(ComplexStep complexStep) {
+        for (Step st : complexStep.subscenario) {
+            st.accept(this);
+        }
+        depthCount += 1;
+    }
+
+    @Override
+    public void visit(MainScenario mainScenario) {
+        for (Step step : mainScenario.steps) {
+            step.accept(this);
+        }
+    }
+}
