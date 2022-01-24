397a263fc9a9f45938c5c3ef7c7584ad7fdcddf5
mdziecielski <dziecielski.m@gmail.com>
2022-01-24 19:47:31 +0100

    Add javadoc for CustomDepthVisitor and DepthCountingVisitor

--- src/main/java/pl/put/poznan/checker/logic/CustomDepthVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/CustomDepthVisitor.java
@@ -3,6 +3,9 @@ package pl.put.poznan.checker.logic;
 import java.util.Collection;
 import java.util.LinkedList;
 
+/**
+ * Creates scenario with custom depth (provided by user)
+ */
 public class CustomDepthVisitor implements ScenarioVisitor {
     private int depth;
     private int currentDepth = 1;
@@ -12,11 +15,20 @@ public class CustomDepthVisitor implements ScenarioVisitor {
         this.depth = depth;
     }
 
+    /**
+     * Visit step without subscenario
+     * @param simpleStep A SimpleStep Class object, step without sub-scenario
+     */
     @Override
     public void visit(SimpleStep simpleStep) {
         customSteps.add(new CustomStep(simpleStep.text));
     }
 
+    /**
+     * Visit step with subscenario
+     * Change value of currentDepth accordingly
+     * @param complexStep A ComplexStep Class object, step containing a sub-scenario
+     */
     @Override
     public void visit(ComplexStep complexStep) {
         customSteps.add(new CustomStep(complexStep.text));
@@ -30,6 +42,10 @@ public class CustomDepthVisitor implements ScenarioVisitor {
         }
     }
 
+    /**
+     * Begins visit for mainScenario and calls visit method for every step
+     * @param mainScenario class instance representing scenario
+     */
     @Override
     public void visit(MainScenario mainScenario) {
         for (Step step: mainScenario.getSteps()) {
--- src/main/java/pl/put/poznan/checker/logic/DepthCountingVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/DepthCountingVisitor.java
@@ -1,11 +1,23 @@
 package pl.put.poznan.checker.logic;
 
+/**
+ * Counts scenario depth
+ */
 public class DepthCountingVisitor implements ScenarioVisitor {
     int depthCount = 1;
 
+    /**
+     * Visit step without scenario
+     * Doesn't affect depthCount
+     * @param simpleStep A SimpleStep Class object, step without sub-scenario
+     */
     @Override
     public void visit(SimpleStep simpleStep) { }
 
+    /**
+     * Visit step with a scenario and increase depth
+     * @param complexStep A ComplexStep Class object, step containing a sub-scenario
+     */
     @Override
     public void visit(ComplexStep complexStep) {
         for (Step st : complexStep.subscenario) {
@@ -14,6 +26,10 @@ public class DepthCountingVisitor implements ScenarioVisitor {
         depthCount += 1;
     }
 
+    /**
+     * Begins visit for mainScenario and calls visit method for every step
+     * @param mainScenario class instance representing scenario
+     */
     @Override
     public void visit(MainScenario mainScenario) {
         for (Step step : mainScenario.getSteps()) {
