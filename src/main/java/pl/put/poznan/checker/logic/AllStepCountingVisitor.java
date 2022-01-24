1cabf8a1de00bc4ca7823ea28f1f1a29128d42b7
mdziecielski <dziecielski.m@gmail.com>
2022-01-24 19:51:07 +0100

    Add javadoc for AllStepCountingVisitor

--- src/main/java/pl/put/poznan/checker/logic/AllStepCountingVisitor.java
+++ src/main/java/pl/put/poznan/checker/logic/AllStepCountingVisitor.java
@@ -1,13 +1,26 @@
 package pl.put.poznan.checker.logic;
 
+/**
+ * Counts number of steps
+ */
 public class AllStepCountingVisitor implements ScenarioVisitor {
     int stepCount = 0;
 
+    /**
+     * Visit step without subscenario
+     * Increment stepCount
+     * @param simpleStep A SimpleStep Class object, step without sub-scenario
+     */
     @Override
     public void visit(SimpleStep simpleStep) {
         stepCount += 1;
     }
 
+    /**
+     * Visit step with subscenario
+     * Increment stepCount
+     * @param complexStep A ComplexStep Class object, step containing a sub-scenario
+     */
     @Override
     public void visit(ComplexStep complexStep) {
         for (Step st : complexStep.subscenario) {
@@ -16,6 +29,10 @@ public class AllStepCountingVisitor implements ScenarioVisitor {
         stepCount += 1;
     }
 
+    /**
+     * Begins visit for mainScenario and calls visit method for every step
+     * @param mainScenario Scenario to be analysed
+     */
     @Override
     public void visit(MainScenario mainScenario) {
         for (Step step : mainScenario.getSteps()) {
